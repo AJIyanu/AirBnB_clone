8f12e68f3b0cf75ef49d5d06405bea175f77d9a6
AJIyanu <aderemijoseph@live.com>
2022-08-14 21:32:47 +0100

    data exhusted

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ created at date time
 can also update from json
 this is all for now
 """
+
 import uuid
 from datetime import datetime
 
@@ -25,3 +26,22 @@ class BaseModel(object):
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
+
+    def __str__(self):
+        """
+        This is a string representation of my class
+        It returns the class name id and dictionary
+        """
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """
+        This is a save method to save shits
+        It also updates the save time to update at
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        This returns a dictionary serialized for json
+        """
