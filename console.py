287885db4caaa99a93826ebd3599607800aff1b9
AJIyanu <aderemijoseph@live.com>
2022-08-14 06:29:58 +0100

    cmd on hold

---
+++ console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is the console necessary for the project
+This is a commnent
+"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
